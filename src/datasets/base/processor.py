e9b0e9c1de0ddcd3f4b1a222439e0330f7845283
johng149 <johnguo149@gmail.com>
2024-03-26 20:19:18 -0400

    Add helpers and tests

--- src/datasets/base/processor.py
+++ src/datasets/base/processor.py
@@ -2,23 +2,38 @@ from src.tokenizers.models.info import Info
 from src.common.models.verification import Verification
 from src.common.models.args_info import ArgsInfo
 from src.common.services.verification import verify_args
+from torch.utils.data import DataLoader
+from typing import Optional
+
 
 class Processor:
 
     def __init__(self, info: Info):
         self.info = info
-        self.tokenizer = info["tokenizer"]
-        self.pad_idx = info["pad_idx"]
-        self.bos_idx = info["bos_idx"]
-        self.eos_idx = info["eos_idx"]
-        self.mask_idx = info["mask_idx"]
-        self.vocab_size = info["vocab_size"]
+        self.tokenizer = info.tokenizer
+        self.pad_idx = info.pad_idx
+        self.bos_idx = info.bos_idx
+        self.eos_idx = info.eos_idx
+        self.mask_idx = info.mask_idx
+        self.vocab_size = info.vocab_size
 
     def process(self, **kwargs):
         raise NotImplementedError
-    
+
     def validate_args(self, **kwargs) -> Verification:
         return verify_args({}, **kwargs)
 
     def encode(self, sample):
         raise NotImplementedError
+
+    def seq2seq(self) -> Optional[DataLoader]:
+        return None
+
+    def supports_seq2seq(self) -> bool:
+        return False
+
+    def causal(self) -> Optional[DataLoader]:
+        return None
+
+    def supports_causal(self) -> bool:
+        return False
--- test/datasets/base/processor_test.py
+++ test/datasets/base/processor_test.py
@@ -4,10 +4,33 @@ from src.datasets.base.processor import Processor
 import pytest
 
 
+def test_processor_init():
+    mock_tokenizer_info = MagicMock()
+    mock_tokenizer = "some_tokenizer"
+    mock_tokenizer_info.tokenizer = mock_tokenizer
+    pad_idx = 0
+    bos_idx = 1
+    eos_idx = 2
+    mask_idx = 3
+    vocab_size = 4
+    mock_tokenizer_info.pad_idx = pad_idx
+    mock_tokenizer_info.bos_idx = bos_idx
+    mock_tokenizer_info.eos_idx = eos_idx
+    mock_tokenizer_info.mask_idx = mask_idx
+    mock_tokenizer_info.vocab_size = vocab_size
+    processor = Processor(mock_tokenizer_info)
+    assert processor.info == mock_tokenizer_info
+    assert processor.tokenizer == mock_tokenizer
+    assert processor.pad_idx == pad_idx
+    assert processor.bos_idx == bos_idx
+    assert processor.eos_idx == eos_idx
+    assert processor.mask_idx == mask_idx
+    assert processor.vocab_size == vocab_size
+
+
 @patch("src.common.services.verification.verify_args")
 def test_processor_validate_args(mock_verify_args):
     mock_tokenizer_info = MagicMock()
-    mock_tokenizer_info.__getitem__.side_effect = lambda key: None
     processor = Processor(mock_tokenizer_info)
     processor.validate_args(some_arg="some_value")
     mock_verify_args.call_count == 1
@@ -16,7 +39,6 @@ def test_processor_validate_args(mock_verify_args):
 
 def test_processor_process():
     mock_tokenizer_info = MagicMock()
-    mock_tokenizer_info.__getitem__.side_effect = lambda key: None
     processor = Processor(mock_tokenizer_info)
     with pytest.raises(NotImplementedError):
         processor.process()
@@ -24,7 +46,20 @@ def test_processor_process():
 
 def test_processor_encode():
     mock_tokenizer_info = MagicMock()
-    mock_tokenizer_info.__getitem__.side_effect = lambda key: None
     processor = Processor(mock_tokenizer_info)
     with pytest.raises(NotImplementedError):
         processor.encode("some_sample")
+
+
+def test_processor_seq2seq():
+    mock_tokenizer_info = MagicMock()
+    processor = Processor(mock_tokenizer_info)
+    assert processor.seq2seq() == None
+    assert not processor.supports_seq2seq()
+
+
+def test_processor_causal():
+    mock_tokenizer_info = MagicMock()
+    processor = Processor(mock_tokenizer_info)
+    assert processor.causal() == None
+    assert not processor.supports_causal()
