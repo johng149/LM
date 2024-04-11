4cb4b9cb750e703685cf7704d44a67c55a71a2dd
johng149 <johnguo149@gmail.com>
2024-04-11 08:30:04 -0400

    Add flags for show browser and verbose

--- unit_tests.bat
+++ unit_tests.bat
@@ -1,7 +1,43 @@
 @echo off
+setlocal
+
+REM Init options
+set verbose=0
+set open_browser=n
+set ask_open_browser=1
+
+REM Parse command line arguments
+:parse
+if "%~1"=="" goto :continue
+if /i "%~1"=="-v" (
+    set verbose=1
+    shift
+    goto :parse
+)
+if /i "%~1"=="-o" (
+    set ask_open_browser=0
+    set open_browser=y
+    shift
+    goto :parse
+)
+if /i "%~1"=="-a" (
+    set ask_open_browser=0
+    set open_browser=%~2
+    shift
+    shift
+    goto :parse
+)
+echo Invalid argument: %~1
+exit /b 1
+
+:continue
 
 REM Run the tests
-coverage run -m pytest
+if "%verbose%"=="1" (
+    coverage run -m pytest -v
+) else (
+    coverage run -m pytest
+)
 
 REM Generate the coverage report
 coverage report
@@ -10,6 +46,9 @@ coverage xml
 
 REM Open htmlcov/index.html in default browser after confirming user wants to do so
 REM by default, assume no
-set open_browser=n
-set /p open_browser=Open coverage report in default browser? (y/n)
-if /i "%open_browser%" EQU "y" start htmlcov/index.html
+if "%ask_open_browser%"=="1" (
+    set /p open_browser=Open coverage report in default browser? (y/n) 
+)
+if /i "%open_browser%"=="y" (
+    start htmlcov\index.html
+)
--- unit_tests.sh
+++ unit_tests.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
+# Initialize options
+verbose=0
+open_browser="n"
+ask_open_browser=1
+
+# Parse command line arguments
+while getopts "vba:" opt; do
+    case ${opt} in
+        v)
+            verbose=1
+            ;;
+        b)
+            ask_open_browser=0
+            open_browser="y"
+            ;;
+        a)
+            ask_open_browser=0
+            open_browser="$OPTARG"
+            ;;
+        \?)
+            echo "Usage: unit_tests.sh [-v] [-b] [-a y|n]"
+            echo "Options:"
+            echo "  -v: Verbose output"
+            echo "  -b: Open coverage report in default browser"
+            echo "  -a y|n: Automatically open coverage report in default browser"
+            exit 1
+            ;;
+    esac
+done
+
 # Run the tests
-coverage run -m pytest
+if [ "$verbose" -eq 1]; then
+    coverage run -m pytest -v
+else
+    coverage run -m pytest
+fi
 
 # Generate the coverage report
 coverage report
@@ -9,9 +43,9 @@ coverage html
 coverage xml
 
 # Open htmlcov/index.html in default browser after confirming user wants to do so
-# by default, assume no
-open_browser="n"
-read -p "Do you want to open the coverage report in your default browser? (y/N) " open_browser
+if [ "$ask_open_browser" -eq 1 ]; then
+    read -p "Do you want to open the coverage report in your default browser? (y/N) " open_browser
+fi
 if [ "$open_browser" == "y" ]; then
     open htmlcov/index.html
 fi
