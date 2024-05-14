f14ecdf25211738eba81877c705ddfa958c25bd1
johng149 <jg843@cornell.edu>
2024-05-14 17:57:59 -0400

    Reimplemented masking

--- src/datasets/utils/masking.py
+++ src/datasets/utils/masking.py
@@ -1,19 +1,79 @@
 import torch
 from torch import Tensor
+from typing import Tuple
 
-def causal_mask(input_tensor: Tensor, pad_idx: int) -> Tensor:
-    """
-    Create causal mask for the input tensor. The mask
-    is a lower triangular matrix with the diagonal and
-    has the appropriate columns zeroed out for positions
-    that are padding tokens
 
-    @param input_tensor: the input tensor of shape 
-        (batch_size, seq_len)
-    @return: causal mask of shape (batch_size, seq_len, seq_len)
+def process_tokens(tokens: Tensor, pad_idx: int) -> Tuple[Tensor, Tensor]:
     """
-    batch_size, seq_len = input_tensor.shape
-    token_is_not_pad = (input_tensor != pad_idx)
-    padding_mask = token_is_not_pad.unsqueeze(-1).expand(-1, -1, seq_len).transpose(-2, -1)
-    causal_mask = torch.tril(torch.ones(seq_len, seq_len, device=input_tensor.device)).unsqueeze(0).expand(batch_size, -1, -1)
-    return padding_mask & causal_mask.bool()
+    Process tokens to create information needed for creating masks.
+
+    @param tokens: the tokens to process, shape (batch_size, seq_len)
+    @param pad_idx: the index of the padding token
+    @returns: a tuple containing the number of tokens in each sequence
+    that is not padding and a boolean tensor indicating which tokens
+    are not padding, shape (batch_size, seq_len)
+    """
+    token_is_not_pad = tokens != pad_idx
+    token_lens = torch.sum(token_is_not_pad, dim=1)
+    return token_lens, token_is_not_pad
+
+
+def self_attn_pad_mask(is_not_pad: Tensor) -> Tensor:
+    """
+    @param not_is_pad: bool tensor of shape (batch_size x seq_len)
+        where True indicates a non-padding token
+    @return: bool tensor of shape (batch_size x seq_len x seq_len)
+        where the value at position (i, j, k) is True if the
+        attention matrix value at that same position should NOT be masked
+    """
+    batch_size, l = is_not_pad.shape
+    return is_not_pad.unsqueeze(-1).expand(-1, -1, l).transpose(-2, -1)
+
+
+def cross_attn_pad_mask(kv_is_not_pad: Tensor, q_is_not_pad: Tensor):
+    """
+    @param not_is_pad_kv: bool tensor of shape (batch_size x seq_len_kv)
+        where True indicates a non-padding token
+    @param not_is_pad_q: bool tensor of shape (batch_size x seq_len_q)
+        where True indicates a non-padding token
+    @return: bool tensor of shape (batch_size x seq_len_q x seq_len_kv)
+        where the value at position (i, j, k) is True if the attention
+        matrix value at that same position should not be masked
+    """
+    batch_size, l_kv = kv_is_not_pad.shape
+    _, l_q = q_is_not_pad.shape
+    return kv_is_not_pad.unsqueeze(-1).expand(-1, -1, l_q).transpose(-2, -1)
+
+
+def causal_self_attn_mask(samples: Tensor):
+    """
+    @param samples: a tensor of shape (batch_size, seq_len)
+        containing the input data
+    @return: a tensor of shape (batch_size, seq_len, seq_len)
+        where the value at position (i, j, k) is True if the
+        attention matrix value at that same position should not be masked
+    """
+    batch_size, l = samples.shape
+    return (
+        torch.tril(torch.ones((l, l), device=samples.device))
+        .unsqueeze(0)
+        .expand(batch_size, -1, -1)
+        .bool()
+    )
+
+
+def sdpa_flip(mask: Tensor) -> Tensor:
+    """
+    @param mask: a tensor of shape (batch_size, seq_len, seq_len)
+        where the value at position (i, j, k) is True if the
+        attention matrix value at that same position should not be masked
+    @return: a tensor of shape (batch_size, seq_len, seq_len)
+        where the value at position (i, j, k) is False if the
+        attention matrix value at that same position should not be masked
+
+    The reason for this is that scaled dot product attention (sdpa) expects
+    a mask where True values indicate positions to mask out, while the
+    masks we create indicate positions to NOT mask out. This function
+    flips the mask so that it can be used with sdpa.
+    """
+    return torch.logical_not(mask)
--- test/datasets/utils/masking_test.py
+++ test/datasets/utils/masking_test.py
@@ -1,32 +1,114 @@
-from src.datasets.utils.masking import causal_mask
+from src.datasets.utils.masking import (
+    process_tokens,
+    self_attn_pad_mask,
+    cross_attn_pad_mask,
+    causal_self_attn_mask,
+    sdpa_flip,
+)
+from torch import Tensor
 import torch
+import pytest
 
 
-def test_causal_mask_no_padding():
-    input_tensor = torch.tensor(
+def test_process_tokens():
+    tokens = torch.tensor([[1, 2, 3, -1, -1], [4, 5, -1, 6, 7], [1, 2, 3, 4, 5]])
+    pad_idx = -1
+    expected_lens = torch.tensor([3, 4, 5])
+    expected_is_not_pad = torch.tensor(
         [
-            [1, 2, 3, 4, 5],
-            [6, 7, 8, 9, 1],
+            [True, True, True, False, False],
+            [True, True, False, True, True],
+            [True, True, True, True, True],
         ]
     )
-    pad_idx = 0
-    mask = causal_mask(input_tensor, pad_idx)
-    expected = torch.tensor(
+    token_lens, token_is_not_pad = process_tokens(tokens, pad_idx)
+    assert torch.equal(token_lens, expected_lens)
+    assert torch.equal(token_is_not_pad, expected_is_not_pad)
+
+
+def test_self_attn_pad_mask():
+    is_not_pad = torch.tensor(
+        [
+            [True, True, False],
+            [True, False, False],
+            [True, False, True],
+            [False, False, True],
+            [False, False, False],
+        ]
+    )
+    expected_output = torch.tensor(
+        [
+            [[True, True, False], [True, True, False], [True, True, False]],
+            [[True, False, False], [True, False, False], [True, False, False]],
+            [[True, False, True], [True, False, True], [True, False, True]],
+            [[False, False, True], [False, False, True], [False, False, True]],
+            [[False, False, False], [False, False, False], [False, False, False]],
+        ]
+    )
+    output = self_attn_pad_mask(is_not_pad)
+    assert torch.equal(output, expected_output)
+
+
+def test_cross_attn_pad_mask():
+    kv_is_not_pad = torch.tensor(
+        [
+            [True, True, False, False],
+            [True, False, False, False],
+            [True, False, True, True],
+        ]
+    )
+    q_is_not_pad = torch.tensor(
+        [
+            [True, False, False],
+            [True, False, True],
+            [False, False, True],
+        ]
+    )
+    expected_output = torch.tensor(
         [
             [
-                [1, 0, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 1, 0, 0],
-                [1, 1, 1, 1, 0],
-                [1, 1, 1, 1, 1],
+                [True, True, False, False],
+                [True, True, False, False],
+                [True, True, False, False],
             ],
             [
-                [1, 0, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 1, 0, 0],
-                [1, 1, 1, 1, 0],
-                [1, 1, 1, 1, 1],
+                [True, False, False, False],
+                [True, False, False, False],
+                [True, False, False, False],
+            ],
+            [
+                [True, False, True, True],
+                [True, False, True, True],
+                [True, False, True, True],
             ],
         ]
     )
-    assert torch.equal(mask, expected)
+    output = cross_attn_pad_mask(kv_is_not_pad, q_is_not_pad)
+    assert torch.equal(output, expected_output)
+
+
+def test_causal_self_attn_mask():
+    samples = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    expected_output = torch.tensor(
+        [
+            [
+                [True, False, False],
+                [True, True, False],
+                [True, True, True],
+            ],
+            [
+                [True, False, False],
+                [True, True, False],
+                [True, True, True],
+            ],
+        ]
+    )
+    output = causal_self_attn_mask(samples)
+    assert torch.equal(output, expected_output)
+
+
+def test_sdpa_flip():
+    mask = torch.rand(3, 4, 4) > 0.5
+    expected_output = ~mask
+    output = sdpa_flip(mask)
+    assert torch.equal(output, expected_output)
