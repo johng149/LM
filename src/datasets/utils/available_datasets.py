2d97b596b0218479d2eb9a6ed0ee0c4a638545ae
johng149 <jg843@cornell.edu>
2024-05-15 08:34:25 -0400

    Add available datasets dict

---
+++ src/datasets/utils/available_datasets.py
@@ -0,0 +1,3 @@
+from src.datasets.services.tiny_shakespeare import TinyShakespeareProcessor
+
+available_datasets = {"tiny_shakespeare": TinyShakespeareProcessor}
---
+++ test/datasets/utils/available_datasets_test.py
@@ -0,0 +1,5 @@
+from src.datasets.utils.available_datasets import available_datasets
+
+
+def test_available_datasets_num():
+    assert len(available_datasets) == 1
