f7772ae10e69eb36513d0d8074b4ea2c315e9164
johng149 <jg843@cornell.edu>
2024-05-23 08:23:35 -0400

    Implemented wmt19 en zh dataset

--- src/datasets/services/wmt19_en_zh.py
+++ src/datasets/services/wmt19_en_zh.py
@@ -73,3 +73,71 @@ class WMT19EnZhProcessor(Processor):
 
         train_data.save_to_disk(train_path)
         val_data.save_to_disk(val_path)
+
+    def collate_causal_fn(
+        self,
+    ) -> Callable[
+        [List[List[int] | List[Tensor]], int, int, int], Tuple[Tensor, Tensor]
+    ]:
+        def collate_fn(
+            batch: List[dict],
+            bos_idx: int,
+            eos_idx: int,
+            pad_idx: int,
+        ) -> Tuple[Tensor, Tensor]:
+            bos_idx = torch.tensor([bos_idx], dtype=torch.long)
+            eos_idx = torch.tensor([eos_idx], dtype=torch.long)
+            combined = []
+            for x in batch:
+                en = x["en_encoded"]
+                zh = x["zh_encoded"]
+                if not isinstance(en, Tensor):
+                    en = torch.tensor(en)
+                if not isinstance(zh, Tensor):
+                    zh = torch.tensor(zh)
+                concatenated = torch.cat([bos_idx, en, eos_idx, bos_idx, zh, eos_idx])
+                combined.append(concatenated)
+            source = [x[:-1] for x in combined]
+            target = [x[1:] for x in combined]
+
+            source = torch.nested.nested_tensor(source)
+            target = torch.nested.nested_tensor(target)
+
+            source = torch.nested.to_padded_tensor(source, pad_idx)
+            target = torch.nested.to_padded_tensor(target, pad_idx)
+
+            len_not_pad, is_not_pad = process_tokens(source, pad_idx)
+            source_causal_mask = causal_self_attn_mask(source)
+            source_pad_mask = self_attn_pad_mask(is_not_pad)
+            source_mask = combine_masks_before_flip(source_causal_mask, source_pad_mask)
+
+            return source, source_mask, target
+
+        return collate_fn
+
+    def causal(
+        self, dataset_path: str, type: DataloaderType, batch_size: int, **kwargs
+    ) -> DataLoader | None:
+        path = self.format_dataset_path(dataset_path, type)
+
+        data = load_from_disk(path)
+        collate = self.collate_causal_fn()
+        dl = DataLoader(
+            data,
+            batch_size=batch_size,
+            collate_fn=lambda x: collate(x, self.bos_idx, self.eos_idx, self.pad_idx),
+        )
+
+        return dl
+
+    def causal_verify_args(self, **kwargs) -> Tuple[List[Verification], bool]:
+        return verify_args(
+            {
+                "batch_size": ArgInfo(
+                    level=ParamLevel.REQUIRED,
+                    type=int,
+                    description="The batch size for the DataLoader",
+                )
+            },
+            **kwargs
+        )
