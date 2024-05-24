506112e515bc8a2c54dc21f423e406a7d6c4e0bd
johng149 <jg843@cornell.edu>
2024-05-24 10:04:22 -0400

    Fix naive inference test

--- test/nn/base/architecture_test.py
+++ test/nn/base/architecture_test.py
@@ -10,30 +10,10 @@ def test_architecture():
         arch.init_kwargs()
 
 
-def test_architecture_naive_decode_batch_fail():
-    arch = Architecture()
-    batch, seq, max_len = 2, 10, 20
-    x = torch.randn(batch, seq)
-    strat = MagicMock()
-    with pytest.raises(AssertionError):
-        arch.naive_inference(x, strat, max_len)
-
-
-def test_architecture_naive_decode_padding_fail():
-    arch = Architecture()
-    batch, seq, max_len = 1, 10, 20
-    x = torch.randint(0, 100, (batch, seq))
-    strat = MagicMock()
-    strat.pad_id.return_value = 0
-    x[0][-1] = strat.pad_id()
-    with pytest.raises(AssertionError):
-        arch.naive_inference(x, strat, max_len)
-
-
 def test_architecture_naive_decode_not_implemented():
     arch = Architecture()
     max_len = 20
     x = torch.tensor([[1, 2, 3]])
     strat = MagicMock()
     strat.pad_id.return_value = 0
-    assert arch.naive_inference(x, strat, max_len) == None
+    assert arch.naive_inference(x, strat=strat, max_len=max_len) is None
