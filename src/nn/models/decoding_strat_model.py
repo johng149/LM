2d185abd5fd063717ec3e35f1782331dee237d9d
johng149 <jg843@cornell.edu>
2024-05-27 11:02:48 -0400

    Implemented greedy dag decoding

--- src/nn/models/decoding_strat_model.py
+++ src/nn/models/decoding_strat_model.py
@@ -29,7 +29,7 @@ class AutoregressiveStrategy(DecodingStrategy):
     def decode(self, output: Tensor) -> Tensor:
         """
         This is more specific than a generic decoding strategy,
-        it must be that the output tensor's batch dimension
+        it must be that the input tensor's batch dimension
         is equal to the output tensor's batch dimension.
 
         @param output: the output tensor, shape (batch_size, vocab_size)
@@ -41,3 +41,16 @@ class AutoregressiveStrategy(DecodingStrategy):
 class DAGStrategy(DecodingStrategy):
     def __init__(self, info: Info, device: Union[device, str]):
         super().__init__(info, device)
+
+    def decode(self, transition: Tensor, emissions: Tensor) -> Tensor:
+        """
+        This is more specific than a generic decoding strategy.
+        Given a transition matrix and emissions matrix, we follow
+        path through the graph to produce, in parallel, the
+        decoded sequence (non-autoregressive).
+
+        @param transition: the transition matrix, shape (batch_size, vertex_count, vertex_count)
+        @param emissions: the emissions matrix, shape (batch_size, vertex_count, vocab_size)
+
+        """
+        raise NotImplementedError()
--- src/nn/services/decoding_strat.py
+++ src/nn/services/decoding_strat.py
@@ -1,5 +1,7 @@
 from torch import Tensor
 from src.nn.models.decoding_strat_model import AutoregressiveStrategy
+from src.nn.models.decoding_strat_model import DAGStrategy
+import torch
 
 
 class GreedyAutoregressiveStrategy(AutoregressiveStrategy):
@@ -10,3 +12,40 @@ class GreedyAutoregressiveStrategy(AutoregressiveStrategy):
         batch_size, vocab_size = output.shape
         greedy = output.argmax(dim=-1)
         return greedy.reshape(batch_size, -1)
+
+
+class GreedyDAGStrategy(DAGStrategy):
+    def __init__(self, info=None, device=None):
+        self.info = info  # we don't actually need the info for this strategy
+        self.device = device
+
+    def decode_single(self, transition: Tensor, emissions: Tensor):
+        vertex_count, vocab_size = emissions.shape
+
+        tokens = torch.argmax(emissions, dim=1)
+        edges = torch.argmax(transition, dim=1)
+
+        edges[edges == 0] = vertex_count
+
+        i = 0
+        output = [tokens[i].item()]
+        while i < vertex_count:
+            i = edges[i].item()
+            if i >= vertex_count:
+                break
+            output.append(tokens[i].item())
+
+        return torch.tensor(
+            output, device=self.device if self.device is not None else emissions.device
+        )
+
+    def decode(self, transition: Tensor, emissions: Tensor):
+        batch_size, vertex_count, vocab_size = emissions.shape
+
+        # unfortunately, DAG decoding doesn't support parallelizing
+        # across the batch dimension so we'll need a loop
+        decoded = []
+
+        for i in range(batch_size):
+            decoded.append(self.decode_single(transition[i], emissions[i]))
+        return torch.stack(decoded)
