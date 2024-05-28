0c4dba6dc6fafcbabbd3ccee26a098c9febc39b6
johng149 <jg843@cornell.edu>
2024-05-28 07:39:52 -0400

    Fixed loss function signature

--- src/loss_fns/services/loss_fns.py
+++ src/loss_fns/services/loss_fns.py
@@ -4,6 +4,9 @@ from src.loss_fns.services.dag_loss_efficient import dag_loss_adapter
 
 available_loss_fns = {
     "cross_entropy": create_cross_entropy_loss_fn,
-    "brute_force_dag_loss": lambda: brute_force_dag_loss,
-    "dag_loss": lambda: dag_loss_adapter,
+    # below we use dummy lambda that takes in an `x`
+    # (the tokenizer), so that it follows the same
+    # pattern as the other entries in the dictionary
+    "brute_force_dag_loss": lambda x: brute_force_dag_loss,
+    "dag_loss": lambda x: dag_loss_adapter,
 }
